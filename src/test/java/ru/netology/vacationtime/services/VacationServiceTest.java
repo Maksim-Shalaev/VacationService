3ef6fb988c8b2af492ce80e28a4d674818dd565f
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-05-09 00:42:47 +0300

    Updated pom.xml

--- src/test/java/ru/netology/vacationtime/services/VacationServiceTest.java
+++ src/test/java/ru/netology/vacationtime/services/VacationServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.vacationtime.services;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,19 +10,29 @@ public class VacationServiceTest {
 
     @ParameterizedTest
     @CsvSource({
-            "2, 100000, 60000, 150000",
-//            "3, 10000, 3000, 20000",
-//            "0, 0, 10000, 30000",
-//            "1, 1000, 0, 11000"
+            "2, 100000, 60000, 150000"
     })
-
     public void testFromTask1(int expected, int income, int expense, int threshold) {
         VacationService service = new VacationService();
-        //int expected = 2;
         int actual = service.calculate(income, expense, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
+
+//    @CsvSource({
+//            "2, 100000, 60000, 150000",
+//            "3, 10000, 3000, 20000",
+//            "0, 0, 10000, 30000",
+//            "1, 1000, 0, 11000"
+//    })
+//    @Test
+//    public void testFromTask1() {
+//        VacationService service = new VacationService();
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
 //    @Test
 //    public void testFromTask2() {
 //        VacationService service = new VacationService();
